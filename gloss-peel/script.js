cb84a862008ad1ff9632e070384e78e2f6cb9f76
davidcarusso <carusso479@gmail.com>
2025-10-12 07:08:23 -0300

    cambios en archivos js, falta modificar

--- gloss-peel/script.js
+++ gloss-peel/script.js
@@ -658,5 +658,15 @@ if (!Element.prototype.closest) {
 // FIN DEL SCRIPT
 // ============================================
 
-console.log('%c🌟 Voelkeep Landing Page', 'color: #C94C7C; font-size: 20px; font-weight: bold;');
-console.log('%cDesarrollado por SixSight Studio - UADE Team Two', 'color: #8B1E5A; font-size: 12px;');
+// --- IGNORE ---
+
+// function firmaEquipoUade() {
+// // Mensajes de consola
+// console.log('%c🌟 Voelkeep Web', 'color: #C94C7C; font-size: 20px; font-weight: bold;');
+// console.log('%cDesarrollado por SixSight Studio - UADE Team Two', 'color: #8B1E5A; font-size: 12px;');
+// console.log('%cEquipo:', 'color: #C94C7C; font-size: 14px; font-weight: bold;');
+// console.log('%cCardoso, Tomas Francisco\nCarle, Santiago\nCarusso, David Ismael\nCáceres, Mariana\nCórdoba Bourilhon, Micaela\nDi Yorio, Juan', 'color: #8B1E5A; font-size: 12px;');
+
+// }
+
+// firmaEquipoUade();
